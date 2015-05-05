12747eb402aa1c980557e4b0f36217ccc4e3087a
celic <cceli@codequarry.net>
2015-05-05 15:01:05 -0400

    Finishes converter

--- csv_converter.rb
+++ csv_converter.rb
@@ -1,13 +1,64 @@
 # Converts .csv to a proper .txt
 
-seasons = ['200708', '200809', '200910', '201011', '201112', '201213', '201314', '201415']
+seasons = ['200708', '200809', '201011', '201112']
 positions = ['forwards', 'defense', 'goalies']
 
-seasons.each do |season|
+positions.each do |position|
 
-	positions.each do |position|
+	param_x = []
+	param_y = []
 
-		param_x = []
-		param_y = []
+	seasons.each do |season|
+
+		# Open CSV and modify content
+		IO.readlines("files/#{season}_#{position}.csv").each do |line|
+
+			content = line.split(',')
+
+			content[0] = param_x.count + 1
+			x_content = content[0..-2].join
+			y_content = content[-1].chomp[0..-2]
+
+			param_x << x_content
+			param_y << [param_y.count + 1, y_content].join
+		end
 	end
+
+	txt = File.new("files/#{position}_DAT.txt", "w")
+
+	txt.write("#PARAMETERS\n")
+	txt.write("param p:= #{param_x[0].split.count};\n")
+	txt.write("param n:= #{param_y.count};\n")
+	txt.write("param c:= .1;\n")
+
+	txt.puts
+	txt.puts
+
+	txt.write("#ALL X DATA\n")
+	txt.write("param X :\n")
+	(1..param_x[0].split.count).each do |n|
+		txt.write("\t#{n}")
+	end
+	txt.write("\n")
+	txt.write(":=\n")
+
+	param_x.each do |x|
+		txt.write("#{x}\n")
+	end
+
+	txt.write(";\n")
+
+
+	txt.puts
+	txt.puts
+
+	txt.write("#ALL Y DATA\n")
+	txt.write("param Y :=\n")
+
+	param_y.each do |y|
+		txt.write("#{y}\n")
+	end
+
+	txt.write(";\n")
 end
+
